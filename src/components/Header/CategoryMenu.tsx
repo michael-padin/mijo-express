24ef0442fd311c0393b2004e07eec2ecb134b7b7
Michael Padin <padinmichael201@gmail.com>
2023-11-13 14:16:25 +0800

    Feat: add components

---
+++ src/components/Header/CategoryMenu.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Button } from "../ui/button";
+
+const serviceCategories = [
+	"Home Services",
+	"Pet Care",
+	"Gardening and Outdoor Tasks",
+	"Childcare",
+	"Community Events",
+	"Transportation",
+	"Emergency Assistance",
+	"Technology Assistance",
+	"Wellness and Fitness",
+	"Art and Creativity",
+	"Language Exchange",
+	"Community Support",
+	"Local Business Support",
+	"Educational Support",
+	"Sustainable Living",
+];
+
+const CategoryMenu = () => {
+	return (
+		<div className="border border-y">
+			<div className="max-w-5xl m-auto">
+				<div className="flex gap-2 flex-wrap py-4">
+					{serviceCategories.map((name, idx) => (
+						<div className="" key={idx}>
+							<Button variant="secondary">{name}</Button>
+						</div>
+					))}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default CategoryMenu;
