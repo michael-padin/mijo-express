312aca33ee470b0f45525a0fd7d328c4dad2a91a
Michael Padin <padinmichael201@gmail.com>
2024-01-06 23:24:59 +0800

    Add MongoDB connection and utility functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,28 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import mongoose, { ConnectionStates } from "mongoose";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+const connection = { isConnected: false };
+
+export const connectToDB = async () => {
+  try {
+    if (connection.isConnected) {
+      console.log("Already connected to MongoDB");
+      return;
+    }
+
+    if (!process.env.MONGODB_URI) {
+      console.log("MongoDB URI is not defined");
+      return;
+    }
+
+    const db = await mongoose.connect(process.env.MONGODB_URI);
+
+    connection.isConnected = db.connections[0].readyState === 1;
+    console.log("Connected to MongoDB");
+  } catch (error) {}
+};
