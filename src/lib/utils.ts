6f9d69ee9c5e221f2e7a1732532e44eecce08ae8
Michael Padin <padinmichael201@gmail.com>
2024-01-06 23:30:42 +0800

    Update tailwind.config.js and components.json

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,6 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
-import mongoose, { ConnectionStates } from "mongoose";
+import mongoose from "mongoose";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
