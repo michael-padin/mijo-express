bd94370d3f6246a8089ea1567d96c0c483b9b5ea
Michael Padin <padinmichael201@gmail.com>
2024-01-08 20:24:01 +0800

    Add image domains to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (config) => {
-        config.resolve.fallback = {
-            "aws4": false,
-        }
-        return config
-    }
-}
+  images: {
+    domains: ["randomuser.me", "via.placeholder.com"],
+  },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      aws4: false,
+    };
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
