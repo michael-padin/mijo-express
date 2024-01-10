64682b87282619941f162df895ad581811c7894b
kyle-salaysay <foursalaysay@gmail.com>
2024-01-11 01:39:32 +0800

    change registerSchema

--- src/app/(auth)/register/components/registerForm.tsx
+++ src/app/(auth)/register/components/registerForm.tsx
@@ -1,5 +1,7 @@
 "use client";
 import { Button } from "@/components/ui/button";
+import { Loader2 } from "lucide-react";
+
 import {
   Form,
   FormControl,
@@ -7,16 +9,68 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
+  FormDescription,
 } from "@/components/ui/form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Checkbox } from "@/components/ui/checkbox";
+import { useState } from "react";
+
 import { Input } from "@/components/ui/input";
 import React from "react";
+import { Loader2Icon } from "lucide-react";
+
 import { useForm } from "react-hook-form";
+import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { toast } from "sonner";
 import { useRouter } from "next/navigation";
 import { MeInputAddress } from "@/components/me/me-input-address";
-import { Loader2 } from "lucide-react";
+import { Separator } from "@/components/ui/separator";
 import { RegisterData, RegisterSchema } from "../data/schema";
+
+//THIS SHOULD BE FETCH FROM ADMIN
+
+export async function getSkill() {
+  try {
+    const res = await fetch("/api/skills");
+    return await res.json();
+  } catch (error) {
+    console.log("Failed");
+  }
+}
+
+const skills = [
+  {
+    id: "recents",
+    label: "Recents",
+  },
+  {
+    id: "home",
+    label: "Home",
+  },
+  {
+    id: "applications",
+    label: "Applications",
+  },
+  {
+    id: "desktop",
+    label: "Desktop",
+  },
+  {
+    id: "downloads",
+    label: "Downloads",
+  },
+  {
+    id: "documents",
+    label: "Documents",
+  },
+] as const;
 
 const RegisterForm = () => {
   const router = useRouter();
@@ -37,6 +91,13 @@ const RegisterForm = () => {
 
     toast.success("Account created successfully");
     router.push("/login");
+  };
+
+  const [selectedRole, setSelectedRole] = useState("provider");
+
+  const handleRoleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedRole(event.target.value);
+    // Additional logic based on the selected role can be added here
   };
 
   return (
@@ -85,6 +146,105 @@ const RegisterForm = () => {
               </FormItem>
             )}
           />
+          <FormField
+            control={form.control}
+            name="contact"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Contact Number</FormLabel>
+                <FormControl>
+                  <Input placeholder="09*********" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="role"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>User Type</FormLabel>
+                <Select
+                  onValueChange={(value) => {
+                    setSelectedRole(value);
+                    field.onChange(value);
+                  }}
+                  defaultValue={field.value}
+                >
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Set Role Type" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    <SelectItem value="customer">Customer</SelectItem>
+                    <SelectItem value="provider">Provider</SelectItem>
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          {selectedRole === "provider" && (
+            <>
+              <Separator />
+              <FormField
+                control={form.control}
+                name="skills"
+                render={() => (
+                  <FormItem>
+                    <div className="mb-4">
+                      <FormLabel className="text-base">Skills</FormLabel>
+                      <FormDescription>
+                        Select the skills you that you have.
+                      </FormDescription>
+                    </div>
+                    {skills.map((item) => (
+                      <FormField
+                        key={item.id}
+                        control={form.control}
+                        name="skills"
+                        render={({ field }) => {
+                          return (
+                            <FormItem
+                              key={item.id}
+                              className="flex flex-row items-start space-x-3 space-y-0"
+                            >
+                              <FormControl>
+                                <Checkbox
+                                  checked={field.value?.includes(item.id)}
+                                  onCheckedChange={(checked) => {
+                                    return checked
+                                      ? field.onChange([
+                                          ...field.value,
+                                          item.id,
+                                        ])
+                                      : field.onChange(
+                                          field.value?.filter(
+                                            (value) => value !== item.id
+                                          )
+                                        );
+                                  }}
+                                />
+                              </FormControl>
+                              <FormLabel className="text-sm font-normal">
+                                {item.label}
+                              </FormLabel>
+                            </FormItem>
+                          );
+                        }}
+                      />
+                    ))}
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Separator />
+            </>
+          )}
+
           <FormField
             control={form.control}
             name="password"
--- src/app/(auth)/register/data/schema.ts
+++ src/app/(auth)/register/data/schema.ts
@@ -4,8 +4,21 @@ export const RegisterSchema = z
   .object({
     fullName: z.string().min(3),
     email: z.string().email(),
-    password: z.string().min(6),
     address: z.string().min(3),
+    contact: z.string().refine(
+      (value) => {
+        const contactNumberRegex = /^09\d{9}$/; // Pattern for a 10-digit number starting with '09'
+        return contactNumberRegex.test(value);
+      },
+      {
+        message: "Invalid contact number format",
+      }
+    ),
+    role: z.enum(["provider", "customer"]),
+    skills: z.array(z.string()).refine((value) => value.some((item) => item), {
+      message: "You have to select at least one item.",
+    }),
+    password: z.string().min(6),
     confirmPassword: z.string().min(6),
   })
   .refine((data) => data.password === data.confirmPassword, {
