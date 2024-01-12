6db4836777bbe6af2f2324a902fc5953c4a06866
Michael G. Padin <padinmichael201@gmail.com>
2024-01-13 02:58:56 +0800

    Add AppReviewCard component and fix some tiny ui in landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 import { Loader2Icon } from "lucide-react";
+
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 import Image from "next/image";
 import Link from "next/link";
-import landingImage from "../../public/images/cover.svg";
 
 import { MapPinned } from "lucide-react";
 import { Phone } from "lucide-react";
@@ -16,10 +16,23 @@ import {
   CardContent,
   CardHeader,
   CardDescription,
+  CardTitle,
 } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "@/components/ui/button";
+import App from "next/app";
+import Autoplay from "embla-carousel-autoplay";
+import AppReviewCard from "./components/app-review-card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
 interface CardContent {
   avatarSrc: string;
@@ -29,31 +42,18 @@ interface CardContent {
   cardContent: string;
 }
 
-const cardContentData: CardContent[] = [
-  {
-    avatarSrc: "https://github.com/shadcn.png",
-    avatarAlt: "@shadcn",
-    avatarFallback: "CN",
-    cardDescription: "John Kyle",
-    cardContent:
-      "Absolutely thrilled with the service! The team went above and beyond to ensure a seamless and successful experience.",
-  },
-  {
-    avatarSrc: "https://example.com/user2.png",
-    avatarAlt: "@user2",
-    avatarFallback: "U2",
-    cardDescription: "Jane Doe",
-    cardContent:
-      "Exceptional service! Timely and professional throughout the entire process.",
-  },
-  {
-    avatarSrc: "https://example.com/user3.png",
-    avatarAlt: "@user3",
-    avatarFallback: "U3",
-    cardDescription: "Alex Johnson",
-    cardContent:
-      "The service exceeded my expectations! Courteous and skilled professionals who ensured a smooth experience.",
-  },
+const testReviewAppData = Array.from({ length: 10 }, (_, i) => ({
+  profileImg: `https://randomuser.me/api/portraits/women/${i + 1}.jpg`,
+  fullName: `User ${i + 1}`,
+  address: `Address ${i + 1}`,
+  description: `This is a description for User ${i + 1}.`,
+  createdAt: new Date(),
+}));
+
+const homeNavLinks = [
+  { label: "Home", href: "/" },
+  { label: "Login", href: "/login" },
+  { label: "Register", href: "/register" },
 ];
 
 export default function Home() {
@@ -67,101 +67,110 @@ export default function Home() {
     // <div className="flex h-screen items-center justify-center">
     //   <Loader2Icon className="animate-spin" size={30} />
     // </div>
-    <div className="w-screen overflow-x-hidden">
-      <div className="h-[60vh] w-full">
-        <Image
-          src={landingImage}
-          alt="landing-image"
-          objectFit="cover"
-          className="h-[60vh] w-screen"
-        />
-        <div className="-mt-96 mb-40 flex h-32 w-screen flex-row justify-between p-10">
+    <div className=" overflow-x-hidden">
+      <div className="relative h-[60vh] w-full border border-b">
+        <div className="flex justify-between  p-5 ">
           <div className="flex flex-row justify-between">
-            <h3 className="text-3xl font-bold text-yellow-500">
-              Mijo <span className="text-black">Express</span>
-            </h3>
+            <h1 className="text-2xl font-black">
+              <span className="text-primary">MiJo </span> Express
+            </h1>
           </div>
-          <ul className="flex flex-row gap-0">
-            <li className="flex h-16 w-32 scale-105 cursor-pointer items-center justify-center text-black hover:text-yellow-500">
-              <Link href="">Home</Link>
-            </li>
-            <li className="flex h-16 w-32 scale-105 cursor-pointer items-center justify-center text-black hover:text-yellow-500">
-              <Link href="login">Login</Link>
-            </li>
-            <li className="flex h-16 w-32 cursor-pointer items-center justify-center border-2 border-yellow-500 text-yellow-500 hover:scale-105 hover:text-yellow-500">
-              <Link href="register">Register</Link>
-            </li>
+          <ul className="z-50 flex items-center gap-2">
+            {homeNavLinks.map((item, index) => (
+              <li
+                key={item.href}
+                className={`${
+                  item.href === "/register"
+                    ? cn(buttonVariants({ variant: "default" }))
+                    : cn(buttonVariants({ variant: "ghost" }))
+                }`}
+              >
+                <Link href={item.href}>{item.label}</Link>
+              </li>
+            ))}
           </ul>
         </div>
-      </div>
+        <Image
+          src="images/cover.svg"
+          alt="landing-image"
+          fill
+          objectFit="contain"
+        />
+        <div className="absolute -bottom-24 left-0 right-0 m-auto flex   max-w-[1024px] justify-center gap-5 overflow-x-hidden">
+          {/* <Separator /> */}
 
-      {/* ABOUT SECTION HERE */}
-      <div className="flex w-screen flex-col justify-start gap-5 overflow-x-hidden p-16">
-        <Separator />
-        <h2 className="text-8xl font-black text-yellow-500">
-          About <span className="text-black">our Service</span>
-        </h2>
-        <p>
-          Mijo Express is an online platform where customers effortlessly find
-          the service providers they need.
-          <br />
-          With a powerful search function and transparent service provider
-          profiles, users can quickly locate qualified professionals.
-          <br />
-          The platforms user-friendly design, secure booking system, and
-          efficient communication features redefine the process of <br />
-          connecting customers with reliable service providers.
-        </p>
+          <Card className="bg-muted">
+            <CardHeader>
+              <CardTitle className="text-xl font-black text-primary">
+                About{" "}
+                <span className="text-muted-foreground ">Our Service</span>
+              </CardTitle>
+            </CardHeader>
+            <CardContent className="flex text-muted-foreground">
+              <p>
+                Mijo Express is an online platform where customers effortlessly
+                find the service providers they need. With a powerful search
+                function and transparent service provider profiles, users can
+                quickly locate qualified professionals. The platforms
+                user-friendly design, secure booking system, and efficient
+                communication features redefine the process of connecting
+                customers with reliable service providers.
+              </p>
+            </CardContent>
+          </Card>
+        </div>
       </div>
 
       {/* REVIEW CARDS SECTION */}
-      <div className="mb-10 w-screen overflow-x-hidden px-10">
-        <Separator />
-        <h2 className="my-10 text-8xl font-black text-yellow-500">
-          Rate <span className="text-black">and Review</span>
-        </h2>
-        <div className="flex flex-row items-center justify-start gap-10">
-          {cardContentData.map((item, index) => (
-            <Card key={index} className="h-[400px] w-1/4">
-              <CardHeader>
-                <Avatar>
-                  <AvatarImage src={item.avatarSrc} alt={item.avatarAlt} />
-                  <AvatarFallback>{item.avatarFallback}</AvatarFallback>
-                </Avatar>
-                <CardDescription>{item.cardDescription}</CardDescription>
-              </CardHeader>
-              <Separator />
-              <CardContent>
-                <p className="m-2">{item.cardContent}</p>
-              </CardContent>
-            </Card>
-          ))}
-        </div>
+      <div className=" m-auto mb-10 mt-32 max-w-[1024px]">
+        <h1 className="p-6 text-center text-xl font-black text-primary">
+          Rate <span className="text-muted-foreground ">and Review</span>
+        </h1>
+        <Carousel
+          className="w-full"
+          plugins={[
+            Autoplay({
+              delay: 2000,
+            }),
+          ]}
+        >
+          <CarouselContent>
+            {testReviewAppData.map((item, index) => (
+              <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/4">
+                <AppReviewCard key={index} {...item} />
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
       </div>
 
       {/* FOOTER SECTION */}
-      <div className="w-screen overflow-x-hidden bg-yellow-500">
+      <div className="overflow-x-hidden bg-primary">
         {/* CONTACTS */}
-        <div className="flex flex-row justify-around p-10">
-          <div className="flex flex-row items-center justify-center gap-3">
-            <MapPinned />
-            <div className="flex flex-col gap-1">
-              <h5>Find Us</h5>
-              <a href="">Poblacion, Argao, Cebu</a>
+        <div className="m-auto max-w-[1024px]">
+          <div className="flex flex-row justify-between py-10">
+            <div className="flex flex-row items-center justify-center gap-3">
+              <MapPinned />
+              <div className="flex flex-col gap-1">
+                <h5>Find Us</h5>
+                <a href="">Poblacion, Argao, Cebu</a>
+              </div>
             </div>
-          </div>
-          <div className="flex flex-row items-center justify-center gap-3">
-            <Phone />
-            <div className="flex flex-col gap-1">
-              <h5>Call Us</h5>
-              <p>09465398213 / 481-6593</p>
+            <div className="flex flex-row items-center justify-center gap-3">
+              <Phone />
+              <div className="flex flex-col gap-1">
+                <h5>Call Us</h5>
+                <p>09465398213 / 481-6593</p>
+              </div>
             </div>
-          </div>
-          <div className="flex flex-row items-center justify-center gap-3">
-            <MailOpen />
-            <div className="flex flex-col gap-1">
-              <h5>Email Us</h5>
-              <p>mijo-express@gmail.com</p>
+            <div className="flex flex-row items-center justify-center gap-3">
+              <MailOpen />
+              <div className="flex flex-col gap-1">
+                <h5>Email Us</h5>
+                <p>mijo-express@gmail.com</p>
+              </div>
             </div>
           </div>
         </div>
